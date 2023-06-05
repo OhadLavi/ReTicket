63888ff90b1debafacd8cdefde1b6228a9bae6da
Ohad <81176492+OhadLavi@users.noreply.github.com>
2023-06-05 23:53:57 +0300

    Delete utils directory

--- utils/authenticate.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    checkAuthenticated: function(req, res, next){
-        if(req.isAuthenticated()){
-            return next();
-        }
-        res.render("../public/views/home.ejs", {user: null});
-        //res.redirect("/");
-    }
-}
