43eed65ccd51dbf649bfa878252a9afff0699f80
Ohad Lavi <ulalavi@gmail.com>
2023-06-15 17:41:30 +0300

    Added Google Speech Recognition and string similarity to the search

--- backend/src/routers/event.router.js
+++ backend/src/routers/event.router.js
@@ -1,25 +1,77 @@
 const express = require('express');
 const router = express.Router();
-const sample_events = require('../data/events'); // Assuming you have an events data file
-const { Event } = require('../models/event.model'); // Assuming you have an event model
+const sample_events = require('../data/events');
+const { Event } = require('../models/event.model');
 const asyncHandler = require('express-async-handler');
+const multer = require('multer');
+const speech = require('@google-cloud/speech');
+const upload = multer();
+const path = require('path');
+const stringSimilarity = require('string-similarity');
+const levenshtein = require('fast-levenshtein');
+const natural = require('natural');
+const fs = require('fs');
 
 router.get("/", asyncHandler(async (req, res) => {
   const events = await Event.find();
   res.json(events);
 }));
 
-router.get("/search/:searchTerm", asyncHandler(async (req, res) => {
-  const searchRegex = new RegExp(req.params.searchTerm, 'i');
-  const events = await Event.find({ 
-    $or: [
-      { name: { $regex: searchRegex } },
-      { location: { $regex: searchRegex } },
-      { venue: { $regex: searchRegex } },
-      { city: { $regex: searchRegex } }
-    ]
+router.get("/search/:searchTerm?", asyncHandler(async (req, res) => {
+  const { searchTerm } = req.params;
+
+  if (!searchTerm) {
+    const allEvents = await Event.find();
+    return res.json(allEvents);
+  }
+
+  let similarityScores = [];
+  const searchTermWords = searchTerm.toLowerCase().replace(/[^\w\s]/gi, ' ').split(' ');
+  const allEvents = await Event.find();
+  const similarityThreshold = 0.8;
+  const weights = { name: 0.6, location: 0.2, venue: 0.2 };
+  let maxScoreEvent = { event: null, score: 0 };
+  const similarEvents = allEvents.filter(event => {
+    const { name, location, venue } = event;
+    const valuesToSearch = { name, location, venue };
+    let maxSimilarity = 0;
+    Object.entries(valuesToSearch).forEach(([key, value]) => {
+      const words = value.toLowerCase().replace(/[^\w\s]/gi, ' ').split(' ');
+      words.forEach(word => {
+        const checkSimilarity = (term) => {
+          const distance = natural.LevenshteinDistance(term, word);
+          let similarity = 1 - distance / Math.max(term.length, word.length);
+          if (similarity > 0.75 && (key === 'location' || key === 'venue')) {
+            maxSimilarity = Math.max(maxSimilarity, similarity);
+          } else {
+            similarity = weights[key] * similarity;
+            maxSimilarity = Math.max(maxSimilarity, similarity);
+          }
+          const similarityScore = `Similarity between "${term}" and "${word}" in ${key}: ${similarity}`;
+          similarityScores.push(similarityScore);
+          return similarity;
+        };
+        searchTermWords.forEach(checkSimilarity);
+        if (searchTermWords.length > 1) {
+          checkSimilarity(searchTerm.toLowerCase().replace(/[^\w\s]/gi, ' '));
+        }
+      });
+    });
+    if (maxSimilarity > maxScoreEvent.score) {
+      maxScoreEvent = { event, score: maxSimilarity };
+    }
+    return maxSimilarity >= similarityThreshold;
   });
-  res.json(events);
+
+  if (similarEvents.length === 0) {
+    if (maxScoreEvent.event) {
+      similarEvents.push(maxScoreEvent.event);
+    } else {
+      return res.status(500).json({ message: "No results" });
+    }
+  }
+  
+  res.json(similarEvents);
 }));
 
 
@@ -27,6 +79,31 @@ router.get("/id/:eventId", asyncHandler(async (req, res) => {
   const event = await Event.findById(req.params.eventId);
   res.json(event);
 }));
+
+router.post('/transcribeAudio', upload.single('audio'), async (req, res) => {
+  const client = new speech.SpeechClient({
+    projectId: process.env.PROJECT_ID,
+    keyFilename: './keyfile.json'
+});
+  const audio = {
+    content: req.file.buffer.toString('base64'),
+  };
+  const config = {
+    encoding: 'MP3',
+    sampleRateHertz: 16000,
+    languageCode: 'en-US',
+  };
+  const request = {
+    audio: audio,
+    config: config,
+  };
+
+  const [response] = await client.recognize(request);
+  const transcription = response.results
+      .map(result => result.alternatives[0].transcript)
+      .join('\n');
+  res.send({ transcription });
+});
 
 // router.post("/transcribe", upload.single('audio'), async (req, res, next) => {
 //   const fileName = req.file.path;
